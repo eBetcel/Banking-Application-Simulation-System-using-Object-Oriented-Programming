b77e051abf93ecbc2871af90610083f4323beb8f
Emanuel Betcel <emanuelbetcel@gmail.com>
2024-01-23 23:49:13 -0300

    Refactor main function

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ from ContaCorrente import ContaCorrente
 from Cliente import Cliente
 
 def main():
-    cliente = Cliente()
-    conta = ContaCorrente(Cliente("Fulano", "123456789-00"), "1234-5", "1234", 1000)
+    pass    
 
 if __name__ == "__main__":
     main()
